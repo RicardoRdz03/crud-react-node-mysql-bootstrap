b595397e10ecd4e61fcd92a2f051dede6592008c
ricardo <ricardo.arteaga.rdz@gmail.com>
2024-04-29 17:29:10 -0600

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import{useState} from "react";
 import Axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
@@ -31,31 +32,48 @@ function App() {
     });
   }
 
-  getEmpleados();
-
   return (
     <div className='container'>
     <div className="App">
       <div className='lista'>
+        <button class="btn btn-primary" onClick={getEmpleados}>Listar</button>
         {
           empleadosLista.map((val,key)=>{
             return <div className=''>{val.nombre}</div>
           })
         }
       </div>
-      <div class="card text-white bg-primary mb-3">
-  <div class="card-header"><h4 class="card-title text-center">Gestión de empleados</h4></div>
-  <div id='carta-cuerpo' class="card-body text-center">
-        <label>Nombre: <input onChange={(event)=>{setNombre(event.target.value);}} type="text"/></label>
-        <label>Edad: <input onChange={(event)=>{setEdad(event.target.value);}} type="number"/></label>
-        <label>País: <input onChange={(event)=>{setPais(event.target.value);}} type="text"/></label>
-        <label>Cargo: <input onChange={(event)=>{setCargo(event.target.value);}} type="text"/></label>
-        <label>Años: <input onChange={(event)=>{setAños(event.target.value);}} type="number"/></label>
-  </div>
-  <div class="card-footer text-white text-center">
-      <button className='btn btn-dark' onClick={add}>Registrar</button>
-  </div>
-</div>
+
+      <div class="card text-bg-dark mb-3">
+        <div class="card-header"><h5 class="card-title text-center">GESTIÓN DE EMPLEADOS</h5></div>
+        <div class="card-body">
+          
+          <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping">Nombre</span>
+            <input onChange={(event)=>{setNombre(event.target.value);}} type="text" class="form-control" placeholder="Ingrese el nombre" aria-label="Username" aria-describedby="addon-wrapping"/>
+          </div>
+          <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping">Edad</span>
+            <input onChange={(event)=>{setEdad(event.target.value);}} type="number" class="form-control" placeholder="Ingrese la edad" aria-label="Username" aria-describedby="addon-wrapping"/>
+          </div>
+          <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping">País</span>
+            <input onChange={(event)=>{setPais(event.target.value);}} type="text" class="form-control" placeholder="Ingrese un nombre" aria-label="Username" aria-describedby="addon-wrapping"/>
+          </div>
+          <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping">Cargo</span>
+            <input onChange={(event)=>{setCargo(event.target.value);}} type="text" class="form-control" placeholder="Nombre" aria-label="Username" aria-describedby="addon-wrapping"/>
+          </div>
+          <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping">Años</span>
+            <input onChange={(event)=>{setAños(event.target.value);}} type="number" class="form-control" placeholder="Nombre" aria-label="Username" aria-describedby="addon-wrapping"/>
+          </div>
+        </div>
+        <div class="card-footer text-center">
+          <button class="btn btn-success" type='submit' onClick={add}>Registrar</button>
+        </div>
+      </div>
+
     </div>
     </div>
   );
